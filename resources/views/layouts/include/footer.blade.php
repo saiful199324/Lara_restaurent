1a7c6bda0a29bf25da3fa970f5e7eca1722931dd
saiful199324 <molin.dupl.75@gmail.com>
2019-07-23 15:24:08 +0600

    reinitialized files

---
+++ resources/views/layouts/include/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+  <div class="container-fluid">
+
+    <div class="copyright float-right">
+      &copy;
+      <script>
+        document.write(new Date().getFullYear())
+      </script>, made with <i class="material-icons">favorite</i> by
+      <a href="http://dupl2000.com/m_portfolio/" target="_blank">Md. Saiful Islam</a> for a better web.
+    </div>
+  </div>
+</footer>
